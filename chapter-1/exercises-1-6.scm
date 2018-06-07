78e44c394af6cda05f79e096c7263b44686feb94
knockinHeavenDoor <61239289@qq.com>
2018-06-07 23:27:21 +0800

    exercises 1.6

---
+++ chapter-1/exercises-1-6.scm
@@ -0,0 +1,32 @@
+(define (new-if predicate then-clause else-clause)
+	(cond (predicate then-caluse)
+		(else else-caluse)))
+
+(define (improve guess x)
+	(average guess (/ x guess)))
+
+(define (average x y)
+	(/ (+ x y) 2))
+
+(define (good-enough? guess x)
+	(< (abs (- (square guess) x)) 0.001))
+
+(define (square x)
+	(* x x))
+
+(define count 1)
+
+(define (sqrt-iter guess x)
+	(set! count (+ count 1))
+	(display count)
+	(new-if (good-enough? guess x)
+		guess
+		(sqrt-iter (improve guess x) x)))
+
+;;(define (sqrt-iter guess x)
+;;	(cond ((good-enough? guess x)
+;;		guess)
+;;		(else (sqrt-iter (improve guess x) x))))
+
+(define (sqrt x)
+	(sqrt-iter 1.0 x))
