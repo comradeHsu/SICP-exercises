dfa6c835021e64cd982e8e6fc477dc98a2dde0c5
knockinHeavenDoor <61239289@qq.com>
2018-06-08 11:45:52 +0800

    Update exercises-1-6.scm
    add notes

--- chapter-1/exercises-1-6.scm
+++ chapter-1/exercises-1-6.scm
@@ -17,6 +17,7 @@
 (define count 1)
 
 (define (sqrt-iter guess x)
+;;书中的代码是没有以下两行的，这两行是验证代码陷入了无限递归
 	(set! count (+ count 1))
 	(display count)
 	(new-if (good-enough? guess x)
@@ -30,3 +31,7 @@
 
 (define (sqrt x)
 	(sqrt-iter 1.0 x))
+;;当我们调用sqrt函数时，由于new-if定义为了一个过程并且scheme是采用应用序来求值
+;;所以对new-if过程的3个参数求值，其中第三个参数也是一个过程(sqrt_iter (improve guess x) x)) 递归调用自身，导致无限循环直到栈溢出
+;;其实就算是scheme是正则序来求值，正则序完全展开时，会忽略条件，一直展开只至展开到基本元素，所以遇到由条件判断结束的递归就没法结束
+;;所以这里无论是正则序还是应用序都要陷入无限的递归
