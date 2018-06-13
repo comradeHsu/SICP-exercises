95f6f26997280777b0d326a16c8f4853d8c36622
knockinHeavenDoor <61239289@qq.com>
2018-06-13 23:06:46 +0800

    Update exercises-1-11.scm

--- chapter-1/exercises-1-11.scm
+++ chapter-1/exercises-1-11.scm
@@ -1,3 +1,3 @@
 (let (recursion n)
 	(cond ((< n 3) n)
-		(else (+ (recursion (- n 1)) (* (recursion (- n 2) 2) (* (recursion (- n 3) 3))))
+		(else (+ (recursion (- n 1)) (* (recursion (- n 2)) 2) (* (recursion (- n 3)) 3))))
