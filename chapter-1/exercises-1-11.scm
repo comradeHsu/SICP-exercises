b0684126d52edd7d08cd4bcd3f4b73a24b1e6eda
knockinHeavenDoor <61239289@qq.com>
2018-06-13 23:52:35 +0800

    exercises-1-11

--- chapter-1/exercises-1-11.scm
+++ chapter-1/exercises-1-11.scm
@@ -1,3 +1,14 @@
-(let (recursion n)
+(define (recursion n)
 	(cond ((< n 3) n)
-		(else (+ (recursion (- n 1)) (* (recursion (- n 2)) 2) (* (recursion (- n 3)) 3))))
+		(else (+ (recursion (- n 1)) 
+			(* (recursion (- n 2)) 2) 
+			(* (recursion (- n 3)) 3)))))
+
+
+(define (recurs-iter a b c n)
+	(if (= n 0)
+	a
+	(recurs-iter b c (+ (* a 3) (* b 2) c) (- n 1))))
+
+(define (recurs n)
+	(recurs-iter 0 1 2 n))
