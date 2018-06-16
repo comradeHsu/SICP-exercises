5c452c623b3a81c85fe067c9759c79f7f507c548
knockinHeavenDoor <61239289@qq.com>
2018-06-16 23:57:13 +0800

    make paska line data

--- chapter-1/exercises-1-12.scm
+++ chapter-1/exercises-1-12.scm
@@ -1,3 +1,13 @@
 (define (getPaskEle x y)
 	(if (or(= x 1) (= x y))  1
 		(+ (getPaskEle (- x 1) (- y 1)) (getPaskEle x (- y 1)))))
+
+(define paska
+	(lambda(n) 
+		(define data(make-vector n 1))
+		(paska-iter 1 n data)))
+
+(define (paska-iter x n data)
+	(vector-set! data (- x 1) (getPaskEle x n))
+	(if (= x n) data
+	(paska-iter (+ x 1) n data)))
