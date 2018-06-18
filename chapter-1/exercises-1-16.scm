b8d6ef83996cdebab0e9508be8cb024d168a8839
knockinHeavenDoor <61239289@qq.com>
2018-06-18 23:35:39 +0800

    exercises-1-16

---
+++ chapter-1/exercises-1-16.scm
@@ -0,0 +1,20 @@
+;;
+(define (fast-expt b n)
+	(cond ((= n 0) 1)
+		((even? n) (square (fast-expt b (/ n 2))))
+		(else (* b (fast-expt b (- n 1))))))
+
+(define (even? n)
+	(= (remainder n 2) 0))
+
+(define (square x)
+	(* x x))
+
+;;
+(define (iter-expt b n)
+	(expt-iter b n 1))
+
+(define (expt-iter b n sum)
+	(cond ((= n 0) sum)
+		((even? n) (expt-iter b (- n 2) (* sum (square b))))
+		(else (* b (expt-iter b (- n 1) sum)))))
