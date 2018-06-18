0bd2e2e518e3e085b5b328c37f5672368f294cbf
knockinHeavenDoor <61239289@qq.com>
2018-06-18 23:50:22 +0800

    exercises-1-17

---
+++ chapter-1/exercises-1-17.scm
@@ -0,0 +1,19 @@
+
+(define (even? n)
+	(= (remainder n 2) 0))
+
+(define (x a b)
+	(if (= b 0)
+	0
+	(+ a (x a (- b 1)))))
+
+(define (fast-x a b)
+	(cond ((= b 0) 0)
+	((even? b) (double (fast-x a (halve b 2))))
+	(else (+ a (fast-x a (- b 1))))))
+
+(define (double data)
+	(+ data data))
+
+(define (halve data)
+	(/ data 2))
