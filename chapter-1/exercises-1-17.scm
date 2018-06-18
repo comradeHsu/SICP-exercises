d8ad235482d530a3c46ac0ecef8f32fd6bcf1098
knockinHeavenDoor <61239289@qq.com>
2018-06-19 00:23:13 +0800

    fixed bug

--- chapter-1/exercises-1-17.scm
+++ chapter-1/exercises-1-17.scm
@@ -9,7 +9,7 @@
 
 (define (fast-x a b)
 	(cond ((= b 0) 0)
-	((even? b) (double (fast-x a (halve b 2))))
+	((even? b) (double (fast-x a (halve b))))
 	(else (+ a (fast-x a (- b 1))))))
 
 (define (double data)
