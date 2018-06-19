729ea600a9c1da31ab23391d12ce0c63b513599c
knockinHeavenDoor <61239289@qq.com>
2018-06-20 00:17:46 +0800

    delete no using code

--- chapter-1/exercises-1-18.scm
+++ chapter-1/exercises-1-18.scm
@@ -2,16 +2,6 @@
 (define (even? n)
 	(= (remainder n 2) 0))
 
-(define (x a b)
-	(if (= b 0)
-	0
-	(+ a (x a (- b 1)))))
-
-(define (fast-x a b)
-	(cond ((= b 0) 0)
-	((even? b) (double (fast-x a (halve b))))
-	(else (+ a (fast-x a (- b 1))))))
-
 (define (double data)
 	(+ data data))
 
