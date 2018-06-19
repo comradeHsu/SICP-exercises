908621d22f8eca538e4b6225bc237e31699e34e9
knockinHeavenDoor <61239289@qq.com>
2018-06-19 18:19:32 +0800

    原来的写法不是O(log)

--- chapter-1/exercises-1-16.scm
+++ chapter-1/exercises-1-16.scm
@@ -14,7 +14,13 @@
 (define (iter-expt b n)
 	(expt-iter b n 1))
 
+;;(define (expt-iter b n sum)
+;;	(cond ((= n 0) sum)
+;;		((even? n) (expt-iter b (- n 2) (* sum (square b))))
+;;		(else (* b (expt-iter b (- n 1) sum)))))
+
 (define (expt-iter b n sum)
-	(cond ((= n 0) sum)
-		((even? n) (expt-iter b (- n 2) (* sum (square b))))
-		(else (* b (expt-iter b (- n 1) sum)))))
+	(cond ((= n 0) 1)
+		((= n 1) (* b sum))  
+		((even? n) (expt-iter (square b) (/ n 2) sum))
+		(else ((expt-iter b (- n 1) (* b sum))))))
