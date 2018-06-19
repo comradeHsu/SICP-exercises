ad5edbab29c58ed4685539be8375ccc365aaf324
knockinHeavenDoor <61239289@qq.com>
2018-06-19 23:22:32 +0800

    fixed bug

--- chapter-1/exercises-1-16.scm
+++ chapter-1/exercises-1-16.scm
@@ -23,4 +23,18 @@
 	(cond ((= n 0) 1)
 		((= n 1) (* b sum))  
 		((even? n) (expt-iter (square b) (/ n 2) sum))
-		(else ((expt-iter b (- n 1) (* b sum))))))
+		(else (expt-iter b (- n 1) (* b sum)))))
+
+;;another procedure
+(define (expt b n)
+    (define (improve x)
+		(if (even? x)
+   			(/ x 2)
+   			(/ (- x 1) 2)))
+    (define (fast-expt-iter b counter product)
+		(if (= counter 0)
+   			product
+   			(if (even? counter)
+				(fast-expt-iter (square b) (improve counter) product)
+				(fast-expt-iter (square b) (improve counter) (* product b)))))
+    (fast-expt-iter b n 1.0))
