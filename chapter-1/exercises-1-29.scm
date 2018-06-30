2a41ebdde304c72de199d10673f24630ee4f54e9
knockinHeavenDoor <61239289@qq.com>
2018-07-01 01:27:35 +0800

    exercises-1-29

--- chapter-1/exercises-1-29.scm
+++ chapter-1/exercises-1-29.scm
@@ -1,3 +1,35 @@
 (define (integral f a b dx)
-	(define (add-dx) (+ x dx))
+	(define (add-dx x) (+ x dx))
 	(* (sum f (+ a (/ dx 2.0)) add-dx b) dx))
+
+(define (sum term a next b)
+	(if (> a b)
+		0
+		(+ (term a) (sum term (next a) next b))))
+
+(define (inc n) (+ n 1))
+
+(define (sum-square a b)
+	(sum square a inc b))
+
+(define (square n) (* n n))
+
+;;
+(define (simpson f a b n)
+	(define h (/ (- b a) n))
+	(define (next x k) (+ a (* k h)))
+	(* (sum-simpson f a next 0 n) (/ h 3)))
+
+(define (sum-simpson term a next k n)
+	(define (xishu)
+		(cond ((or (= k 0) (= k n)) 1)
+			((even? k) 2)
+			(else 4)))
+	(if (> k n)
+		0
+		(+ (* (xishu) (term a)) (sum-simpson term (next a (+ k 1)) next (+ k 1) n))))
+
+(define (even? n)
+	(= (remainder n 2) 0))
+
+(define cube(lambda(x) (* x x x)))
