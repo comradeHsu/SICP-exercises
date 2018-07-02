88bc750506a90cd20e2f03065652127e18f87c18
knockinHeavenDoor <61239289@qq.com>
2018-07-02 17:50:43 +0800

    Update exercises-1-31.scm

--- chapter-1/exercises-1-31.scm
+++ chapter-1/exercises-1-31.scm
@@ -1,7 +1,26 @@
-;;a
+
+;;a 递归过程
 (define (product fun a next b)
-	(if (> a b) 0
+	(if (> a b) 1
 		(* (fun a) (product fun (next a) next b))))
 
-(define (pie a b)
-	())
+(define (handle a)
+	(if (even? a) 
+		(/ (+ a 2) (+ a 1))
+		(/ (+ a 1) (+ a 2))))
+	
+(define (even? a)
+	(= (remainder a 2) 0))
+	
+(define (next a)
+	(+ a 1))
+	
+(define (pie n)
+	(* (product handle 1 next n) 4))
+	
+;;b 迭代过程
+(define (product-iter fun a next b)
+	(define (iter a result)
+		(if (> a b) 1
+			(iter (next a) (* result (fun a)))))
+	(iter a 1))
