5e06206d55eedafdd9187ec4bb0e38c9fdf1931b
knockinHeavenDoor <61239289@qq.com>
2018-07-04 00:15:12 +0800

    finished exercises-1-35

---
+++ chapter-1/exercises-1-35.scm
@@ -0,0 +1,14 @@
+(define tolerance 0.00001)
+
+;;(let ((<var1><exp1>)...)
+;;	<body>))
+
+(define (fixed-point f first-guess)
+	(define (close-enough? v1 v2)
+		(< (abs (- v1 v2)) tolerance))
+	(define (try guess)
+		(let ((next (f guess)))
+			(if (close-enough? guess next)
+				next
+				(try next))))
+	(try first-guess))
