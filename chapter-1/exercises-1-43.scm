3db74fd8bcaaad0fd7ea52a7360f0e8e0ecef602
knockinHeavenDoor <61239289@qq.com>
2018-07-09 23:34:32 +0800

    exercises-1-43 finished

--- chapter-1/exercises-1-43.scm
+++ chapter-1/exercises-1-43.scm
@@ -1,10 +1,11 @@
 (load "exercises-1-42.scm")
 
 (define (repeated func n)
-	(lambda(x) 
-		(if (= n 1)
-			(func x)
-			(repeated (compose func func) (- n 1)))))
+	(define (inner f cur)
+		(if (< cur n)
+			(compose f (inner f (+ cur 1)))
+			f))
+	(inner func 1))
 
 (define (square x)
 	(* x x))
