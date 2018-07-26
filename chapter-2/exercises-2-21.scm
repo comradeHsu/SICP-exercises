736efb9a80e641fcffa496568dacc1445c4a7c98
knockinHeavenDoor <61239289@qq.com>
2018-07-26 22:32:06 +0800

    finish exercises-2-21

---
+++ chapter-2/exercises-2-21.scm
@@ -0,0 +1,11 @@
+(define (square-list items)
+	(if (null? items)
+		nil
+		(cons (square (car items)) 
+			(square-list (cdr items)))))
+
+(define (square-list items)
+	(map (lambda(x) (* x x)) items))
+
+(define (square x)
+	(* x x))
