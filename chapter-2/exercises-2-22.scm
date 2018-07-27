10e4e1db61c9eed64132096ccedfa0ce594ed774
knockinHeavenDoor <61239289@qq.com>
2018-07-27 11:28:48 +0800

    add exercises-2-22 notes

--- chapter-2/exercises-2-22.scm
+++ chapter-2/exercises-2-22.scm
@@ -5,7 +5,10 @@
 			(iter (cdr things)
 				(cons (square (car things)) answer))))
 	(iter items nil))
-
+;;设items (list 1 2 3)
+;;=> (cons (square 1) nil)
+;;=> (cons (square 2) (cons (square 1) nil))...
+;;=>(3 2 1)
 
 
 (define (square-list items)
@@ -15,3 +18,10 @@
 			(iter (cdr things)
 				(cons answer (square (car things))))))
 	(iter items nil))
+;;设items (list 1 2 3)
+;;=> (cons nil (square 1))
+;;=> (cons (cons nil (square 1)) (square 2))
+;;=> (cons (cons (cons nil (square 1)) (square 2)) (square 3))
+;;=> (cons (cons ((). 1) 4) 9)
+;;=> (cons ((().1).4) 9)
+;;=> (((() . 1) . 4) . 9)
