16b69f2263fe467ea10af45d66535bc5b9d20bdb
knockinHeavenDoor <61239289@qq.com>
2018-07-31 18:38:47 +0800

    finish c and d

--- chapter-2/exercises-2-29.scm
+++ chapter-2/exercises-2-29.scm
@@ -22,7 +22,7 @@
 	(+ (branch-weight (left-branch mobile)) (branch-weight (right-branch mobile))))
 
 (define (branch-weight branch)
-	(let ([branch-leaf (cadr branch)])
+	(let ([branch-leaf (branch-structure branch)])
 		(if (list? branch-leaf)
 			(total-weight branch-leaf)
 			branch-leaf)))
@@ -34,3 +34,41 @@
 		(make-mobile (make-branch 2 3) (make-branch 3 4))))
 	(define a (make-mobile l r))
 	(total-weight a))
+	
+;;c 
+(define (balance? mobile)
+  (newline)
+  (display "evaluating: ")
+  (display mobile)
+  (if (not (list? mobile))
+      (begin
+		(display "not list")
+		#t
+		)
+      (let ([left-one (branch-structure (left-branch mobile))]
+	    [right-one (branch-structure (right-branch mobile))])
+	(newline)
+	(display "inner evaluating:")
+	(display left-one)
+	(display right-one)
+	(if (and (balance? left-one)
+		 (balance? right-one)
+		 (= (* (branch-length (left-branch mobile))
+		       (total-weight left-one))
+		    (* (branch-length (right-branch mobile))
+		       (total-weight right-one))))
+	    #t
+	    #f))))
+
+;;d
+(define (make-mobile-d left right)
+	(list left right))
+
+(define (make-branch-d length structure)
+	(list length structure))
+	
+(define (right-branch mobile)
+  (cdr mobile))
+ 
+(define (branch-structure branch)
+  (cdr branch))
