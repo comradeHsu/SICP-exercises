02e7051d13f0eb7c70e5598c0667fc2b0b340176
knockinHeavenDoor <61239289@qq.com>
2018-08-05 23:53:31 +0800

    copy code

---
+++ chapter-2/exercises-2-40.scm
@@ -0,0 +1,36 @@
+(define (smallest-devisor n)
+	(find-divisor n 2))
+
+(define (find-divisor n test)
+	(cond ((> (square test) n) n)
+		((divides? test n) test)
+		(else (find-divisor n (+ test 1)))))
+
+(define (divides? a b)
+	(= (remainder b a) 0))
+
+(define (square x)
+	(* x x))
+
+(define (prime? n)
+	(= n (smallest-devisor n)))
+
+(define (accumulate op initial sequence)
+	(if (null? sequence)
+		initial
+		(op (car sequence) (accumulate op initial (cdr sequence)))))
+
+(define (flatmap proc seq)
+	(accumulate append '() (map proc seq)))
+
+(define (prime-sum? pair)
+	(prime? (+ (car pair) (cadr pair))))
+
+(define (make-pair-sum pair)
+	(list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define (prime-sum-pairs n)
+	(map make-pair-sum
+		(filter prime-sum?
+			(flatmap (lambda(i) (map (lambda(j) (list i j))
+				(enumerate-interval 1 (- i 1))))))))
