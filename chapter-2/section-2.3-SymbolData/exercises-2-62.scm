4edf6cd623884087fbb952c08f9d5924d28ee074
knockinHeavenDoor <61239289@qq.com>
2018-10-12 17:23:56 +0800

    finished exercises-2-62

---
+++ chapter-2/section-2.3-SymbolData/exercises-2-62.scm
@@ -0,0 +1,17 @@
+(load "exercises-2-61.scm")
+
+(define (union-set set another)
+    (cond ((and (null? set) (null? another))
+            '())
+          ((null? set)
+            another)
+          ((null? another)
+            set)
+          (else
+            (let ((x (car set)) (y (car another)))
+                (cond ((= x y)
+                        (cons x (union-set (cdr set) (cdr another))))
+                      ((< x y)
+                        (cons x (union-set (cdr set) another)))
+                      ((> x y)
+                        (cons y (union-set set (cdr another)))))))))
