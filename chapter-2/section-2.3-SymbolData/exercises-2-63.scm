c9fbfebea237f99cd9c1d528477f628c7e434f44
knockinHeavenDoor <61239289@qq.com>
2018-10-16 18:22:51 +0800

    copy exercises code

--- chapter-2/section-2.3-SymbolData/exercises-2-63.scm
+++ chapter-2/section-2.3-SymbolData/exercises-2-63.scm
@@ -22,3 +22,17 @@
 			(make-tree (entry set) (adjoin-set x (left-branch set)) (right-branch set)))
 		((> x (entry set))
 			(make-tree (entry set) (left-branch set) (adjoin-set x (right-branch set))))))
+			
+(define (tree->list-1 tree)
+	(if (null? tree)
+		'()
+		(append (tree->list-1 (left-branch tree))
+			(cons (entry tree) (tree->list-1 (right-branch tree))))))
+			
+(define (tree->list-2 tree)
+	(define (copy-to-list tree result-list)
+		(if (null? tree)
+			result-list
+			(copy-to-list (left-branch tree)
+				(cons (entry tree) (copy-to-list (right-branch tree) result-list)))))
+	(copy-to-list tree '()))
