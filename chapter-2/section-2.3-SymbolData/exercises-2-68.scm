9509e9ecde3f2b6839afbd1b7e6eada42be046c6
knockinHeavenDoor <61239289@qq.com>
2019-01-01 21:29:03 +0800

    finish exercises 2.68

---
+++ chapter-2/section-2.3-SymbolData/exercises-2-68.scm
@@ -0,0 +1,31 @@
+(load "exercises-2-67.scm")
+
+(define (adjoin-set x set)
+	(cond ((null? set) (list x))
+		((< (weight x) (weight (car set))) (cons x set))
+		(else (cons (car set) (adjoin-set x (cdr set)))))
+
+(define (encode message tree)
+	(if (null? message)
+		'()
+		(append (encode-symbol (car message) tree)
+			(encode (cdr message) tree))))
+
+(define (encode-symbol symbol tree)
+    (cond ((leaf? tree)                                         ; 如果已经到达叶子节点，那么停止积累
+            '())
+          ((symbol-in-tree? symbol (left-branch tree))          ; 符号在左分支(左子树)，组合起 0 
+            (cons 0
+                  (encode-symbol symbol (left-branch tree))))
+          ((symbol-in-tree? symbol (right-branch tree))         ; 符号在右分支(右子树)，组合起 1
+            (cons 1
+                  (encode-symbol symbol (right-branch tree))))
+          (else                                                 ; 给定符号不存在于树，报错
+            (error "This symbol not in tree: " symbol))))
+
+(define (symbol-in-tree? given-symbol tree)
+    (not 
+        (false?
+            (find (lambda (s)                   ; 使用 find 函数，在树的所有符号中寻找给定符号
+                      (eq? s given-symbol))
+                  (symbols tree)))))            ; 取出树中的所有符号
